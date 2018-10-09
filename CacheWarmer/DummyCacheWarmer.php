b6a7a97158fd277729ee4dd8bcc56c7fe00f0afc
pcb <christian.beckmann@webxells.com>
2018-10-09 17:11:22 +0200

    added ServiceSubscriberInterface and Twig\Environment for custom project reasons

--- CacheWarmer/DummyCacheWarmer.php
+++ CacheWarmer/DummyCacheWarmer.php
@@ -4,13 +4,15 @@ namespace FranckRanaivo\Bundle\MultipleInheritBundle\CacheWarmer;
 
 
 use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerInterface;
+use Symfony\Component\DependencyInjection\ServiceSubscriberInterface;
+use Twig\Environment;
 
 /**
  * Class DummyCacheWarmer used to replace some symfony cache warmers.
  *
  * @package FranckRanaivo\Bundle\MultipleInheritBundle\CacheWarmer
  */
-class DummyCacheWarmer implements CacheWarmerInterface {
+class DummyCacheWarmer implements CacheWarmerInterface, ServiceSubscriberInterface {
 
     /**
      * @inheritdoc
@@ -27,4 +29,15 @@ class DummyCacheWarmer implements CacheWarmerInterface {
     {
         // Do nothing
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedServices()
+    {
+        return array(
+            'twig' => '?'.Environment::class,
+        );
+    }
+
 }
